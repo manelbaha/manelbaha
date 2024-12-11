73a853c9b891a5042d3b31a9d53d5a51013d8a7f
manelbaha <manelbaha2005@gmail.com>
2024-12-11 15:41:52 +0100

    Update tp1exo1q2.c

--- tp1exo1q2.c
+++ tp1exo1q2.c
@@ -1,1 +1,22 @@
-hvg
+#include<stdio.h>
+int main(){
+int m[5][5];
+    int i,j,n;
+    printf("donner la premiere diagonale:\n");
+    for(i=0;i<n;i++){
+       for(j=0;j<n;j++){
+          if(i==j){
+        printf("%d \n",m[i][j]);
+          }
+       }      
+    }
+    printf("donner deuxieme diagonale:");
+    for(i=0;i<n;i++){
+       for(j=(n-i-1);j<0;j--){
+          if(i=j){
+            printf("%d \n",m[i][j]);
+          }
+       }                    
+    } 
+    return 0;
+}
